c36538fc9ac145a014665c7016a17f2e50fc7706
vinishakv <158624962+vinishakv@users.noreply.github.com>
2026-03-23 18:07:31 -0400

    fix build errors

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,20 @@
-import resumeImg from '../assets/resume.jpg';
 export default function Contact(){
-    return <section id='contact' className='flex flex-col bg-primary px-5 py-32 text-white'>
+    return(
+        <section id='contact' className='flex flex-col bg-primary px-5 py-32 text-white'>
         <div className='flex flex-col items-center '>
-                <h1 className='text-4xl border-b-4 border-secondary mb-2 w-[100px] font-bold'>Contact</h1>
-                <p className='pb-5'>If you want to discuss more in detail, please Contact me</p>   
-                <p className='py-2'><span className='font-bold'>Email:</span>vinishakoravenkatesalu@gmail.com</p>
-                <p className='py-2'><span className='font-bold'>Phone:</span>+1-(514)-###-####</p>
+            <h1 className='text-4xl border-b-4 border-secondary mb-2 w-[100px] font-bold'>
+                Contact
+            </h1>
+            <p className='pb-5'>
+                If you want to discuss more in detail, please Contact me
+            </p>   
+            <p className='py-2'>
+                <span className='font-bold'>Email:</span>vinishakoravenkatesalu@gmail.com
+            </p>
+            <p className='py-2'>
+                <span className='font-bold'>Phone:</span>+1-(514)-###-####
+            </p>
         </div>
     </section>
-}
+    ); 
+  }
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,13 +9,16 @@ export default function Hero(){
             Instagram: 'https://www.instagram.com/takdu_vini/',
             LinkedIn: 'https://www.linkedin.com/in/vinishakv/' 
         }
-    }
+    };
 
-    return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
+    return(
+        <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
         <div className='md:w-1/2 flex flex-col'>
-            <h1 className='text-white text-6xl font-hero-font'>Hi!! <br/>Am Vinisha
+            <h1 className='text-white text-6xl font-hero-font'>
+                Hi!! <br/>Am Vinisha
                 <p className='text-2xl'>{config.subtitle}</p>
             </h1>
+
             <div className='flex py-10'>
                 <a href={config.social.twitter} className='pr-5 hover:text-white'><BsTwitterX size={40}/></a>
                 <a href={config.social.Instagram} className='pr-5 hover:text-white'><BsInstagram size={40}/></a>
@@ -23,5 +26,6 @@ export default function Hero(){
             </div>
         </div>
         <img className='md:w-1/3' src={HeroImg}/>
-    </section>
+        </section>
+    ); 
 }
